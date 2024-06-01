8635bd4d9d23944d394b7358010bb3d50fe6b862
Taeho <rjsdnxogh55@gmail.com>
2024-06-02 00:09:32 +0900

    Feat: Implement Ingress SSL

--- terraform/nodepool.tf
+++ terraform/nodepool.tf
@@ -27,3 +27,16 @@ resource "ncloud_nks_node_pool" "efk_node_pool" {
         value       = "efk-group"
     }
 }
+
+resource "ncloud_nks_node_pool" "argo_node_pool" {
+    cluster_uuid        = ncloud_nks_cluster.cluster.uuid
+    node_pool_name      = "argo-node-pool"
+    node_count          = 1
+    product_code        = "SVR.VSVR.STAND.C002.M008.NET.SSD.B050.G002"
+    subnet_no_list      = [ncloud_subnet.private_sbn2.id]
+
+    label {
+        key         = "node-group"
+        value       = "argo-group"
+    }
+}
